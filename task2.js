7c3cffdb56409a74e0f49f368298ba72ff896734
pranavmaverick20 <pranavahuja20@gmail.com>
2024-07-27 15:21:24 +0530

    hi

--- task2.js
+++
@@ -1,7 +0,0 @@
-async function start(){
-    let characters =await (await fetch("https://hp-api.onrender.com/api/characters")).json();
-    let spells =await (await fetch("https://hp-api.onrender.com/api/spells")).json();
-    let staff =await (await fetch("https://hp-api.onrender.com/api/characters/staff")).json();
-    let students =await (await fetch("https://hp-api.onrender.com/api/characters/students")).json();
-}
-start();
