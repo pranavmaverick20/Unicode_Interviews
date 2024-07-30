15cdf82247493ca80309c64d0090e457fa5fa6c5
pranavmaverick20 <pranavahuja20@gmail.com>
2024-07-31 01:53:44 +0530

    Added DOM elements

--- task1/task1.js
+++ task1/task1.js
@@ -5,18 +5,30 @@ function charFreq(str, ch) {//this function will count the frequency of ch in st
     }
     return cnt;
 }
-let str = prompt("Enter a String");
-let output = "";//this is the string that will be printed in the output
-str = str.toLowerCase();// to convert string to lower case
-let count = 0;
-for (let i = 97; i <= 122; i++) {
-    let ch = String.fromCharCode(i);//this function converts ASCII value to string
-    let freq = charFreq(str, ch);
-    if (freq > 0) {
-        count++;//this variable counts the number of unique letters 
-        output += ch + ": " + freq + ", ";
+let button = document.querySelector("Button");
+let t = document.querySelector("textarea");
+let res = document.querySelector("p");
+t.addEventListener("click", () => {
+    t.innerText = "";
+    t.value = "";
+})
+button.addEventListener("click", (evt) => {
+    let str = t.value;
+    let output = "";//this is the string that will be printed in the output
+    str = str.toLowerCase();// to convert string to lower case
+    let count = 0;
+    for (let i = 97; i <= 122; i++) {
+        let ch = String.fromCharCode(i);//this function converts ASCII value to string
+        let freq = charFreq(str, ch);
+        if (freq > 0) {
+            count++;//this variable counts the number of unique letters 
+            output += ch + ": " + freq + ", ";
+        }
     }
-}
-output = output.substring(0, output.length - 2);
-output = `${count}\n${output}`;
-console.log(output);
+    output = output.substring(0, output.length - 2);
+    output = `${count}\n${output}`;
+    res.innerText = output;
+    console.log(output);
+
+});
+
