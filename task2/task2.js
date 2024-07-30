76269c9b91e4a1aba1e9112edf2fdcd5d67f7cf1
pranavmaverick20 <pranavahuja20@gmail.com>
2024-07-30 21:20:42 +0530

    Added Bonus task part to fetch characters by id

--- task2/task2.js
+++ task2/task2.js
@@ -1,31 +1,45 @@
 const express = require('express');
 const app = express();
-
-app.get('/',(req,res)=>{
-res.status(200).sendFile('C:/Users/admin/Desktop/unicode tasks/task2/index.html');
+//linking basic html file to show menu
+app.get('/', (req, res) => {
+    res.status(200).sendFile('C:/Users/admin/Desktop/unicode tasks/task2/index.html');
 });
 
+//Now to fetch JSON data from all APIs
 app.get('/api/characters', async (req, res) => {
-    let characters=await (await fetch('https://hp-api.onrender.com/api/characters')).json();
+    let characters = await (await fetch('https://hp-api.onrender.com/api/characters')).json();
     res.status(200).send(characters);
 });
 
 app.get('/api/spells', async (req, res) => {
-    let spells=await (await fetch('https://hp-api.onrender.com/api/spells')).json();
+    let spells = await (await fetch('https://hp-api.onrender.com/api/spells')).json();
     res.status(200).send(spells);
 });
 
 app.get('/api/staff', async (req, res) => {
-    let staff=await (await fetch('https://hp-api.onrender.com/api/characters/staff')).json();
+    let staff = await (await fetch('https://hp-api.onrender.com/api/characters/staff')).json();
     res.status(200).send(staff);
 });
 
 app.get('/api/students', async (req, res) => {
-    let characters=await (await fetch('https://hp-api.onrender.com/api/characters/students')).json();
+    let characters = await (await fetch('https://hp-api.onrender.com/api/characters/students')).json();
     res.status(200).send(characters);
 });
 
-
+/*
+The following method takes in the character id as a request parameter and returns
+the character as a json response
+*/
+app.get('/api/character/:characterID', async (req, res) => {
+    let { characterID } = req.params;
+    console.log(req.params);
+    console.log(characterID)
+    let characters = await (await fetch('https://hp-api.onrender.com/api/characters')).json();
+    const cid = characters.find((c) => {
+        return c["id"] == characterID;
+    });
+    res.send(cid);
+});
 
 
 app.listen(5000, () => {
