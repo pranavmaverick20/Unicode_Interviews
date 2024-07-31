8db04d2c3a6b4554b5bff5b49c1373e207c14797
pranavmaverick20 <pranavahuja20@gmail.com>
2024-08-01 00:15:24 +0530

    Added a way to view DB in json format

--- task3/task3.js
+++ task3/task3.js
@@ -7,24 +7,33 @@ app.use(express.json());
 
 const Student = require('./model.js');
 
-app.post('/api/students',async (req, res) => {
+app.post('/api/students', async (req, res) => {
     try {
         let data = req.body;
-        if (data.house.toLowerCase()=="random") {
+        if (data.house.toLowerCase() == "random") {
             data.house = houseGenerator();
         }
         console.log(data)
-        const student=await Student.create(data);
+        const student = await Student.create(data);
         res.status(200).json(student);
     }
     catch (err) {
         res.status(404).json({ success: false, msg: "Failed to send" })
     }
 });
+//added reading the DB using .find() method
+app.get('/api/students', async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.status(200).json(students);
+    } catch (err) {
+        res.status(404).json({ success: false, msg: "Failed to retrieve students" });
+    }
+});
 
 
 function houseGenerator() {
-    let num = Math.ceil(Math.random() * 4);
+    let num = Math.ceil(Math.random() * 4);//returns a random value between 1 to 4 (inclusive)
     if (num == 1) {
         return "Gryffindor";
     }
@@ -39,7 +48,7 @@ function houseGenerator() {
     }
 }
 
-start=async () => {
+start = async () => {
     try {
         await mongoose.connect("mongodb+srv://pranavahuja20:Unicode_Task3@harrypotterstudents.12oqvj7.mongodb.net/Unicode_Task3?retryWrites=true&w=majority&appName=HarryPotterStudents");
         console.log("Connected to DB!");
