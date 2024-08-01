e72c9271d5e2bcde17eb680ca567e5cabbf19deb
pranavmaverick20 <pranavahuja20@gmail.com>
2024-08-01 17:27:44 +0530

    Renamed folder

--- task3/add.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-axios.post("http://localhost:5000/api/students", {
-    "id": "02",
-    "name": "Hermione Granger",
-    "gender": "female",
-    "house": "Gryffindor",
-    "wizard": false
-});
