de7b7621a7df7461e508d583359ada075ce750b5
pranavmaverick20 <pranavahuja20@gmail.com>
2024-08-01 20:10:32 +0530

    Completed task 4

--- task3and4/task3and4.js
+++ task3and4/task3and4.js
@@ -32,6 +32,25 @@ app.get('/api/students', async (req, res) => {
 });
 
 
+app.delete('/api/students/expell/muggles', async (req, res) => {
+    try {
+        const muggles = await Student.find({ wizard: false });
+        if (!muggles) {
+            return res.status(404).send("No muggles found");
+        }
+        const mugglesid = muggles.map((muggle) => muggle.id);
+        const n = await Student.deleteMany({ id: mugglesid });
+        res.status(200).json({ success: true, msg: "Muggles expelled" });
+    }
+    catch (err) {
+        res.status(404).json({ success: false, msg: "Failed to retrieve students" });
+    }
+    
+});
+
+
+
+
 function houseGenerator() {
     let num = Math.ceil(Math.random() * 4);//returns a random value between 1 to 4 (inclusive)
     if (num == 1) {
@@ -52,14 +71,14 @@ function houseGenerator() {
     try {
         await mongoose.connect("mongodb+srv://Unicode_Task3:Unicode_Task3@harrypotterstudents.12oqvj7.mongodb.net/Unicode_Task3?retryWrites=true&w=majority&appName=HarryPotterStudents");
         console.log("Connected to DB!");
+        app.listen(5000, () => {
+            console.log("Server listening on port 5000");
+
+        });
     }
     catch (error) {
         console.log("Failed to connect");
     }
-    app.listen(5000, () => {
-        console.log("Server listening on port 5000");
-
-    })
 })();
 
 
